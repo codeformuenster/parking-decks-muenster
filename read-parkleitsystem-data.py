ae6313336b9bfb0c46c308c9f9dd3590d521b9bc
ialtrogge <ialtrogge@googlemail.com>
2019-08-20 15:13:43 +0200

    modified file according to the requested changes

--- read-parkleitsystem-data.py
+++ read-parkleitsystem-data.py
@@ -2,51 +2,57 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import os
+import sys
 import datetime
 
 
 # get current date and time
-now=datetime.datetime.today()
+now = datetime.datetime.today()
 date = now.strftime('%Y-%m-%d')
 time = now.strftime('%H:%M')
-german_date = now.strftime('%d.%m.%Y')
 
 # connect with website
-r = requests.get('https://www.stadt-muenster.de/tiefbauamt/parkleitsystem')
-print("status of request (should be 200):")
-print(r.status_code)
+result = requests.get('https://www.stadt-muenster.de/tiefbauamt/parkleitsystem')
+if result.status_code != 200 :
+  print("Request failed with status code: {:d}!".format(result.status_code), file=sys.stderr)
 
-# get content of interest
-soup = BeautifulSoup(r.text, 'html.parser')
-parking_table = soup.find('table')('tr')[1:]
+# get parking table
+soup = BeautifulSoup(result.text, 'html.parser')
+parking_table = soup.find('div', id='parkingList')('table')
+parking_list = None
+for table in parking_table :
+  parking_list = table('tr')[1:]
 
+titles_for_comparison = ['Datum und Uhrzeit', 'PH Theater', 'PP Hörsterplatz', 'PH Alter Steinweg', 'Busparkplatz', 'PP Schlossplatz Nord', 'PP Schlossplatz Süd', 'PH Aegidii', 'PP Georgskommende', 'PH Münster Arkaden', 'PH Karstadt', 'PH Stubengasse', 'PH Bremer Platz', 'PH Engelenschanze', 'PH Bahnhofstraße', 'PH Cineplex', 'PH Stadthaus 3']
 titles = []
 scores = []
 titles.append("Datum und Uhrzeit")
-scores.append(german_date + ", " + time)
+scores.append(date + " " + time)
 
-for i in range(0, len(parking_table)):
-  title = parking_table[i]('td', class_='name')[0].text.strip()
-  score = parking_table[i]('td', class_='freeCount')[0].text.strip()
+for row in parking_list :
+  title = row('td', class_='name')[0].text.strip()
+  score = row('td', class_='freeCount')[0].text.strip()
   titles.append(title)
   scores.append(score)
 print("titles:")
 print(titles)
 print("scores:")
 print(scores)
+if titles != titles_for_comparison :
+  if len(titles) > len(titles_for_comparison) :
+    print("Number of parking decks increased!", file=sys.stderr)
+  elif len(titles) < len(titles_for_comparison) :
+    print("Number of parking decks decreased!", file=sys.stderr)
+  else :
+    print("Order or of parking decks changed!", file=sys.stderr)
 
 # write/append to .csv-file
 filename = date + ".csv"
 print("filename:", filename)
-exists = False
-if os.path.exists(filename) :
-  exists = True
+exists = os.path.exists(filename)
 print("exists:", exists)
 
 writer = csv.writer(open(filename, "a"))
 if not exists :
   writer.writerow(titles)
-  writer.writerow(scores)
-else :
-  writer.writerow(scores)
-
+writer.writerow(scores)
