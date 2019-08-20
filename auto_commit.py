6823cea666b64b4cb3311ad087fec32b8826e3ae
ialtrogge <ialtrogge@googlemail.com>
2019-08-20 20:52:13 +0200

    modified auto_commit.py according to pylint recommendations

--- auto_commit.py
+++ auto_commit.py
@@ -1,19 +1,30 @@
+"""
+automatic commit of daily parkleitsystem data .csv file
+"""
 import os
-import sys
 import datetime
 import config as cfg
 
 
-# get current date and time
-now = datetime.datetime.today()
-yesterday = now - datetime.timedelta(days=1)
-date = yesterday.strftime('%Y-%m-%d')
-filename = date + ".csv"
-filename_long = "data/" + filename
-add_command = 'git add ' + filename_long
-commit_command = 'git commit -m "added parkleitsystem data file"'
-push_command = 'git push https://' + cfg.token + '@github.com/tomsrocket/parking-decks-muenster.git'
+def main():
+    """
+    automatic commit of daily parkleitsystem
+    data to github
+    """
+    # get current date and time
+    now = datetime.datetime.today()
+    yesterday = now - datetime.timedelta(days=1)
+    date = yesterday.strftime('%Y-%m-%d')
+    filename = date + ".csv"
+    filename_long = "data/" + filename
+    add_command = 'git add ' + filename_long
+    commit_command = 'git commit -m "added parkleitsystem data file"'
+    push_command = 'git push https://' + cfg.token + '@github.com/tomsrocket/parking-decks-muenster.git'
 
-os.system(add_command)
-os.system(commit_command)
-os.system(push_command)
+    os.system(add_command)
+    os.system(commit_command)
+    os.system(push_command)
+
+
+if __name__ == "__main__":
+    main()
